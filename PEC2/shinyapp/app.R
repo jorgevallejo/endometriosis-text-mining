ecd00a5ada0a5d603a448a6eff1ccdb447bd16b8
Jorge Vallejo <jorvaor@gmail.com>
2021-04-03 13:02:31 +0200

    Change example in shiny app code
    I am just following the tutorial in "Mastering Shiny".

--- PEC2/shinyapp/app.R
+++ PEC2/shinyapp/app.R
@@ -21,13 +21,7 @@ t_test <- function(x1, x2) {
   )
 }
 
-# Simulate data
-x1 <- rnorm(100, mean = 0, sd = 0.5)
-x2 <- rnorm(200, mean = 0.15, sd = 0.9)
 
-# Compare both variables
-freqpoly(x1, x2)
-cat(t_test(x1, x2))
 
 
 library(shiny)
@@ -36,47 +30,33 @@ library(shiny)
 # User interface
 ui <- fluidPage(
   fluidRow(
-    column(4, 
-           "Distribution 1",
-           numericInput("n1", label = "n", value = 1000, min = 1),
-           numericInput("mean1", label = "µ", value = 0, step = 0.1),
-           numericInput("sd1", label = "σ", value = 0.5, min = 0.1, step = 0.1)
+    column(3, 
+           numericInput("lambda1", label = "lambda1", value = 3),
+           numericInput("lambda2", label = "lambda2", value = 5),
+           numericInput("n", label = "n", value = 1e4, min = 0),
+           actionButton("simulate", "Simulate!")
     ),
-    column(4, 
-           "Distribution 2",
-           numericInput("n2", label = "n", value = 1000, min = 1),
-           numericInput("mean2", label = "µ", value = 0, step = 0.1),
-           numericInput("sd2", label = "σ", value = 0.5, min = 0.1, step = 0.1)
-    ),
-    column(4,
-           "Frequency polygon",
-           numericInput("binwidth", label = "Bin width", value = 0.1, step = 0.1),
-           sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
-    )
-  ),
-  fluidRow(
-    column(9, plotOutput("hist")),
-    column(3, verbatimTextOutput("ttest"))
+    column(9, plotOutput("hist"))
   )
 )
 
 
 # App behaviour
 server <- function(input, output, session){
-  output$hist <- renderPlot({
-    x1 <- rnorm(input$n1, input$mean1, input$sd1)
-    x2 <- rnorm(input$n2, input$mean2, input$sd2)
-    
-    freqpoly(x1, x2, binwidth = input$binwidth, xlim = input$range)
-  }, res = 96)
   
-  output$ttest <- renderText({
-    x1 <- rnorm(input$n1, input$mean1, input$sd1)
-    x2 <- rnorm(input$n2, input$mean2, input$sd2)
-    
-    t_test(x1, x2)
-  })
-  }
+  x1 <- eventReactive(
+    input$simulate, {
+    rpois(input$n, input$lambda1)
+    })
+  x2 <- eventReactive(
+    input$simulate, {
+    rpois(input$n, input$lambda2)
+    })
+  
+  output$hist <- renderPlot({
+    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0,40))
+  }, res = 96)
+}
   
 
 
