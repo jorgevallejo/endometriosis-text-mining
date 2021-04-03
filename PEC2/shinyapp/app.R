fcb78ecdae6e1b98184509dff047ac6a1ae61e9d
Jorge Vallejo <jorvaor@gmail.com>
2021-04-03 13:49:32 +0200

    Input keywords and date range
    Adds input controls for keywords and dates.
    Output is text showing the inputs.

--- PEC2/shinyapp/app.R
+++ PEC2/shinyapp/app.R
@@ -1,61 +1,30 @@
-library(ggplot2)
-
-freqpoly <- function(x1, x2, binwidth = 0.1, xlim = c(-3, 3)) {
-  df <- data.frame(
-    x = c(x1, x2),
-    g = c(rep("x1", length(x1)), rep("x2", length(x2)))
-  )
-  
-  ggplot(df, aes(x, colour = g)) +
-    geom_freqpoly(binwidth = binwidth, size = 1) +
-    coord_cartesian(xlim = xlim)
-}
-
-t_test <- function(x1, x2) {
-  test <- t.test(x1, x2)
-  
-  # use sprintf() to format t.test() results compactly
-  sprintf(
-    "p value: %0.3f\n[%0.2f, %0.2f]",
-    test$p.value, test$conf.int[1], test$conf.int[2]
-  )
-}
-
-
-
-
 library(shiny)
 
 
 # User interface
 ui <- fluidPage(
-  fluidRow(
-    column(3, 
-           numericInput("lambda1", label = "lambda1", value = 3),
-           numericInput("lambda2", label = "lambda2", value = 5),
-           numericInput("n", label = "n", value = 1e4, min = 0),
-           actionButton("simulate", "Simulate!")
-    ),
-    column(9, plotOutput("hist"))
-  )
+  # Enter keywords
+  textInput("keywords",
+            label = "Palabras clave",
+            value = "endometriosis",
+            placeholder = "endometriosis"),
+  # Enter date range
+  dateRangeInput("fechas",
+                 label = "Rango de fechas",
+                 startview = "year",
+                 weekstart = 1, # Monday
+                 language = "es",
+                 separator = "hasta"),
+  textOutput("keyw")
 )
 
 
 # App behaviour
 server <- function(input, output, session){
-  
-  x1 <- eventReactive(
-    input$simulate, {
-    rpois(input$n, input$lambda1)
+  output$keyw <- renderText({
+    c(input$keywords, " desde ", format(input$fechas[1], "%d %b %Y"),
+      " hasta ", format(input$fechas[2], "%d %b %Y"))
     })
-  x2 <- eventReactive(
-    input$simulate, {
-    rpois(input$n, input$lambda2)
-    })
-  
-  output$hist <- renderPlot({
-    freqpoly(x1(), x2(), binwidth = 1, xlim = c(0,40))
-  }, res = 96)
 }
   
 
