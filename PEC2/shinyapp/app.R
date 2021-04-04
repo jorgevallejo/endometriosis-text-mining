86bb0ba761a696aa9438746ed691325d2a7d6948
Jorge Vallejo <jorvaor@gmail.com>
2021-04-04 11:31:25 +0200

    Define end date as current date, and start date as five years prior

--- PEC2/shinyapp/app.R
+++ PEC2/shinyapp/app.R
@@ -1,5 +1,8 @@
 library(shiny)
 
+# Starting value for data range
+# Five years (in days) before current date
+start_date <- Sys.Date()-(5*365.25)
 
 # User interface
 ui <- fluidPage(
@@ -11,6 +14,8 @@ ui <- fluidPage(
   # Enter date range
   dateRangeInput("fechas",
                  label = "Rango de fechas",
+                 start = start_date,
+                 format = "dd-mm-yyyy",
                  startview = "year",
                  weekstart = 1, # Monday
                  language = "es",
@@ -21,10 +26,14 @@ ui <- fluidPage(
 
 # App behaviour
 server <- function(input, output, session){
+  # Generates the text for the query and displays
   output$keyw <- renderText({
-    c(input$keywords, " desde ", format(input$fechas[1], "%d %b %Y"),
-      " hasta ", format(input$fechas[2], "%d %b %Y"))
-    })
+    c(input$keywords, " AND ", format(input$fechas[1], "%Y/%m/%d"),
+      ":",format(input$fechas[2], "%Y/%m/%d"), "[dp]")
+    },
+    sep = "")
+  
+  
 }
   
 
